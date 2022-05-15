6ad7e82064a5750082dddaf37a664cd5b64ef83a
ywyi1992 <ywyi1992@gmail.com>
2022-05-15 16:53:00 +0900

    market add

--- web-page/src/index.jsx
+++ web-page/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Wrap from "./layout/Wrap";
 import Lobby from "./contents/Lobby";
+import Market from "./contents/Market";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,7 +17,7 @@ root.render(
           <Route path="/news" />
           <Route path="/event" />
           <Route path="/sloco" />
-          <Route path="/market" />
+          <Route path="/market" element={<Market />}/>
         </Route>
       </Routes>
     </BrowserRouter>
